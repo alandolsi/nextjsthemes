e88410f77a2a80fcb86903b4ac98b633cc0f4e48
Abdellatif Landolsi <abdellatif@landolsi.de>
2022-03-09 15:51:15 +0100

    change label text

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-primary dark:bg-primary-dark">
-      <h1 className="font-bold text-7xl text-label dark:text-label-dark">Hyperjump Technology</h1>
+      <h1 className="font-bold text-7xl text-label dark:text-label-dark">Landolsi Webdesign</h1>
       <h1 className="mt-10 font-semibold text-3xl text-label dark:text-label-dark">
-        Open source first. Cloud native. DevOps excellence.
+        Webdesign und Entwicklung aus Rosenheim.
       </h1>
     </div>
   )
